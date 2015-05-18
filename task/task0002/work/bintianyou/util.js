54a171f1ad01fbc30b4453f31a94bf33af02d314
bintianyou <447163450@qq.com>
2015-05-18 10:59:29 +0800

    4种判断数据类型方法

---
+++ task/task0002/work/bintianyou/util.js
@@ -0,0 +1,33 @@
+/*4种判断数据类型
+
+var a =2;
+var b ='a';
+var c =[2];
+var d =true;
+var e = function jiaFa(){};
+document.write(" a typeof" +typeof a);
+document.write("<br /> b typeof" +typeof b);
+document.write("<br /> c typeof" +typeof c);
+document.write("<br /> d typeof" +typeof d);
+document.write("<br /> e typeof" +typeof e);
+
+document.write("a instanceof" + a instanceof Number);
+document.write("<br />b instanceof" + b instanceof String);
+document.write("<br />c instanceof" + c instanceof Array);
+document.write("<br />d instanceof" + d instanceof Boolean);
+document.write("<br />e instanceof" + e instanceof Function);
+
+document.write("<br />a constructor" + a.constructor===Number);
+document.write("<br />b constructor" + b.constructor===String);
+document.write("<br />c constructor" + c.constructor===Array);
+document.write("<br />d constructor" + d.constructor===Boolean);
+document.write("<br />e constructor" + e.constructor===Function);
+
+var getType=Object.prototype.toString;
+document.write("getType"+getType.call(a));
+document.write("getType"+getType.call(b));
+document.write("getType"+getType.call(c));
+document.write("getType"+getType.call(d));
+document.write("getType"+getType.call(e));
+*/
+
